8ae1ef93f23ccd84b27eaa5306754548d4a90bc0
Marco <marconovino@gmail.com>
2021-04-09 12:52:00 -0300

    sussy sussy

--- Int3gza.py
+++ Int3gza.py
@@ -233,8 +233,9 @@ async def work(ctx):
     else:
         side1 = user["last_work"] + datetime.timedelta(seconds=600) 
         remaining = side1 - datetime.datetime.now()
-        remainingseconds = remaining.strftime('%M:%S')
-        embed = discord.Embed(title="you cant work yet!", description = f"wait another {remaining} seconds")
+        remainingg = datetime(remaining)
+        remainingseconds = remainingg.strftime('%M:%S')
+        embed = discord.Embed(title="you cant work yet!", description = f"wait another {remainingseconds} seconds")
         await ctx.send(embed=embed)
     
 
